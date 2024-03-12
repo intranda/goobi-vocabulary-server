7bc554a4cdabbed1d1a1ab926e203c751b85c9bf
Dominick Leppich <dominick.leppich@intranda.com>
2024-03-12 14:54:38 +0100

    init: fresh spring boot initializr

---
+++ src/main/java/io/goobi/vocabularyserver/VocabularyServerApplication.java
@@ -0,0 +1,13 @@
+package io.goobi.vocabularyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabularyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocabularyServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/goobi/vocabularyserver/VocabularyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.goobi.vocabularyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocabularyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
