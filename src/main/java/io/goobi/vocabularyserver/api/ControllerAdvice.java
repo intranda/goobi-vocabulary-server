7d0009587969a5802055b322ec22a7319343838f
Dominick Leppich <dominick.leppich@intranda.com>
2024-03-19 13:37:44 +0100

    error: add handler for unsupported REST method

--- src/main/java/io/goobi/vocabularyserver/api/ControllerAdvice.java
+++ src/main/java/io/goobi/vocabularyserver/api/ControllerAdvice.java
@@ -2,6 +2,7 @@ package io.goobi.vocabularyserver.api;
 
 import io.goobi.vocabularyserver.exception.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -11,7 +12,14 @@ public class ControllerAdvice {
     @ResponseBody
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    String notFoundHandler(EntityNotFoundException e) {
+    String entityNotFoundHandler(EntityNotFoundException e) {
+        return e.getMessage();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    String methodNotSupportedHandler(HttpRequestMethodNotSupportedException e) {
         return e.getMessage();
     }
 }
--- src/main/java/io/goobi/vocabularyserver/exception/EntityNotFoundException.java
+++ src/main/java/io/goobi/vocabularyserver/exception/EntityNotFoundException.java
@@ -2,6 +2,6 @@ package io.goobi.vocabularyserver.exception;
 
 public class EntityNotFoundException extends IllegalArgumentException {
     public EntityNotFoundException(String type, long id) {
-        super(type + " with id \"" + id + "\" not found!");
+        super(type + " with id \"" + id + "\" not found");
     }
 }
