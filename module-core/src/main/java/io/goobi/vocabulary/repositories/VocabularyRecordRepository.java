afc3ea2cd08dcc4cba0243e91fc0398bef8d2e3d
Dominick Leppich <dominick.leppich@intranda.com>
2024-05-08 07:48:30 +0200

    fix: remove redundant JPA join

--- module-core/src/main/java/io/goobi/vocabulary/repositories/VocabularyRecordRepository.java
+++ module-core/src/main/java/io/goobi/vocabulary/repositories/VocabularyRecordRepository.java
@@ -16,18 +16,16 @@ public interface VocabularyRecordRepository extends CrudRepository<VocabularyRec
     Page<VocabularyRecordEntity> findByVocabulary_IdAndParentRecordNull(@NonNull Long id, Pageable pageable);
 
     @Query(value = "SELECT vre1_0.id,vre1_0.parent_record_id,vre1_0.vocabulary_id FROM vocabulary_record vre1_0 " +
-            "join vocabulary v1_0 on v1_0.id=vre1_0.vocabulary_id " +
             "left join field_instance f1_0 on vre1_0.id=f1_0.record_id " +
             "left join field_value fv1_0 on f1_0.id=fv1_0.field_instance_id " +
             "left join field_translation t1_0 on fv1_0.id=t1_0.field_value_id " +
-            "where v1_0.id=?1 and t1_0.content like ?2 escape '\\\\'", nativeQuery = true)
+            "where vre1_0.vocabulary_id=?1 and t1_0.content like ?2 escape '\\\\'", nativeQuery = true)
     Page<VocabularyRecordEntity> findRecordsInVocabulary(@NonNull long vocabularyId, @NonNull String value, Pageable pageable);
 
     @Query(value = "SELECT vre1_0.id,vre1_0.parent_record_id,vre1_0.vocabulary_id FROM vocabulary_record vre1_0 " +
-            "join vocabulary v1_0 on v1_0.id=vre1_0.vocabulary_id " +
             "left join field_instance f1_0 on vre1_0.id=f1_0.record_id " +
             "left join field_value fv1_0 on f1_0.id=fv1_0.field_instance_id " +
             "left join field_translation t1_0 on fv1_0.id=t1_0.field_value_id " +
-            "where v1_0.id=?1 and f1_0.field_definition_id=?2 and t1_0.content like ?3 escape '\\\\'", nativeQuery = true)
+            "where vre1_0.vocabulary_id=?1 and f1_0.field_definition_id=?2 and t1_0.content like ?3 escape '\\\\'", nativeQuery = true)
     Page<VocabularyRecordEntity> findRecordsInVocabularyByField(@NonNull long vocabularyId, @NonNull long fieldId, @NonNull String value, Pageable pageable);
 }
