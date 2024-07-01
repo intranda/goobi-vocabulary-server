23947306845713fec63a87a57dfa903a6661cf09
Dominick Leppich <dominick.leppich@intranda.com>
2024-07-01 14:54:07 +0200

    fix: little bugs in Excel import

--- module-core/src/main/java/io/goobi/vocabulary/service/io/excel/ExcelMapperImpl.java
+++ module-core/src/main/java/io/goobi/vocabulary/service/io/excel/ExcelMapperImpl.java
@@ -10,6 +10,8 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -18,13 +20,14 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 
 @Service
 public class ExcelMapperImpl implements ExcelMapper {
     public static final int MAX_CELL_LENGTH = 32767;
     private final TabularRecordExporter tabularRecordExporter;
     private final TabularRecordImporter tabularRecordImporter;
+
+    private Logger logger = LoggerFactory.getLogger(ExcelMapperImpl.class);
 
     public ExcelMapperImpl(TabularRecordExporter tabularRecordExporter, TabularRecordImporter tabularRecordImporter) {
         this.tabularRecordExporter = tabularRecordExporter;
@@ -59,12 +62,22 @@ public class ExcelMapperImpl implements ExcelMapper {
         int numberOfColumns = -1;
         for (int i = 0; i <= sheet.getLastRowNum(); i++) {
             Row row = sheet.getRow(i);
+            if (row == null) {
+                logger.warn("\"null\" row in Excel file, skip processing of remaining file");
+                break;
+            }
             List<String> rowData = new ArrayList<>(row.getLastCellNum() + 1);
             for (int j = 0; j <= row.getLastCellNum(); j++) {
                 String value = null;
                 Cell cell = row.getCell(j);
                 if (cell != null) {
-                    value = cell.getStringCellValue();
+                    switch (cell.getCellType()) {
+                        case STRING -> value = cell.getStringCellValue();
+                        case NUMERIC -> value = String.valueOf(cell.getNumericCellValue());
+                        default ->
+                                throw new IllegalArgumentException("Unsupported cell type \"" + cell.getCellType() + "\"");
+                    }
+
                     if (value.isBlank()) {
                         value = null;
                     }
