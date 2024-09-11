eb4f21c35fdf0b9b2d118d4e25c7d8443a48ecad
Dominick Leppich <dominick.leppich@intranda.com>
2024-09-11 10:39:42 +0200

    task: refactor samples

---
+++ install/install_samples.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+MY_PATH="$(dirname -- "${BASH_SOURCE[0]}")"
+SAMPLES_DIR=$MY_PATH/samples
+CACHE_FILE=$MY_PATH/cache.txt
+
+# Check for parameters
+FAIL=0
+if [ -z $HOST ]; then
+    echo "Missing \"HOST\" parameter!"
+    FAIL=1
+fi
+if [ -z $PORT ]; then
+    echo "Missing \"PORT\" parameter!"
+    FAIL=1
+fi
+if [ -z $TOKEN ]; then
+    echo "Missing \"TOKEN\" parameter!"
+    FAIL=1
+fi
+if [ $FAIL == "1" ]; then
+    echo "Please call this script like the following way (sample call):"
+    echo "$ HOST=localhost PORT=8081 TOKEN=yoursecrettoken $MY_PATH/install_samples.sh"
+    exit 1
+fi
+
+SAMPLE=$1
+if [ -z $SAMPLE ]; then
+    echo "Please chose a sample to install:"
+    ls $SAMPLES_DIR
+    read SAMPLE
+fi
+SAMPLE_PATH=$SAMPLES_DIR/$SAMPLE
+if [ ! -e $SAMPLE_PATH ]; then
+    echo "Sample \"$SAMPLE\" does not exist!"
+    exit 1
+fi
+
+curl_call() {
+    curl --location "$HOST:$PORT/api/v1/$1" \
+        --header 'Content-Type: application/json' \
+        --header "Authorization: Bearer $TOKEN" \
+        --fail \
+        --silent \
+        --data "$2"
+}
+
+for INSTALL_DIR in $(ls $SAMPLE_PATH); do
+    ENDPOINT=$(echo $INSTALL_DIR | cut -d'_' -f2)
+    echo "Installing $ENDPOINT"
+    for ITEM in $(ls $SAMPLE_PATH/$INSTALL_DIR | grep ".json"); do
+        ITEM_NAME=$(echo "$ITEM" | cut -d'.' -f1)
+        ITEM_IDENTIFIER=${ENDPOINT}_"${ITEM_NAME}"
+
+        ID=$(cat $CACHE_FILE | grep $ITEM_IDENTIFIER | cut -d';' -f2)
+        if [ -z $ID ]; then
+            JSON=$(cat $SAMPLE_PATH/$INSTALL_DIR/$ITEM)
+
+            # Replace ID placeholders
+            for CACHE_LINE in $(cat $CACHE_FILE); do
+                TEMPLATE_PLACEHOLDER="{{$(echo $CACHE_LINE | cut -d';' -f1)}}"
+                CACHE_ID=$(echo $CACHE_LINE | cut -d';' -f2)
+                JSON=$(echo $JSON | sed "s/$TEMPLATE_PLACEHOLDER/$CACHE_ID/g")
+            done
+
+            RESULT=$(curl_call $ENDPOINT "$JSON")
+        
+            if [ -z "$RESULT" ]; then
+                echo "Error during creation, exiting!"
+                echo $JSON
+                exit 2
+            fi
+            ID=$(echo $RESULT | jq ".id")
+            echo "$ITEM_IDENTIFIER;$ID" >> $CACHE_FILE
+            echo -e "\tInstalled $ITEM_NAME with ID $ID"
+        else
+            echo -e "\tSkipping $ITEM_NAME, already present with ID $ID"
+        fi
+    done
+done
